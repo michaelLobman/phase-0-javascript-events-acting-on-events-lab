e886a38e5cbb62dd84a988f6a21d59be368ced12
Michael Lobman <michael.lobman@gmail.com>
2021-11-08 11:06:08 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
-const dodger = document.getElementById("dodger");
+const dodger = document.getElementById('dodger');
 
 function moveDodgerLeft() {
 	const leftNumbers = dodger.style.left.replace("px", "");
 	const left = parseInt(leftNumbers, 10);
 
-	if (left > 0 ) {
-		dodger.style.left = `${left - 1}px`;
+	if (left > 0) {
+		dodger.style.left = `${left -1}px`;
 	}
 }
 
-function moveDodgerRight () {
-	const rightNumbers = dodger.style.left.replace("px","");
-	const right = parseInt(rightNumbers, 10);
+function moveDodgerRight() {
+	const leftNumbers = dodger.style.left.replace("px", "");
+	const left = parseInt(leftNumbers, 10);
 
-	if (right < 360 ) {
-		dodger.style.left = `${right + 1}px`;
+	if (left < 360) {
+		dodger.style.left = `${left + 1}px`;
 	}
 }
 
-document.addEventListener("keydown", function (e) {
-	if (e.key === "ArrowLeft") {
+document.addEventListener("keydown", function(e) {
+	if(e.key === "ArrowLeft") {
 		moveDodgerLeft();
-	} else if ( e.key === "ArrowRight") {
+	} else if (e.key === "ArrowRight") {
 		moveDodgerRight();
 	}
 });
