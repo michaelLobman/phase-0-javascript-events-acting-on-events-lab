62fd0cbd8b880690655aee703d245c0d43b21c5e
Michael Lobman <michael.lobman@gmail.com>
2021-11-08 11:03:35 -0500

    commit tree ae2118757864a939b63a3b52723cae1caa6c4219

--- index.js
+++ index.js
@@ -1,27 +1,39 @@
-const dodger = document.getElementById("dodger");
+const dodger = document.getElementById('dodger');
+console.log(dodger);
+
+/*
+document.addEventListener('keydown', function(event) {
+	if (event.key === "ArrowLeft") {
+		const leftNumbers = dodger.style.left.replace("px", "");
+		const left = parseInt(leftNumbers, 10);
+
+		dodger.style.left = `${left - 1}px`;
+	}
+});
+*/
 
 function moveDodgerLeft() {
 	const leftNumbers = dodger.style.left.replace("px", "");
 	const left = parseInt(leftNumbers, 10);
 
-	if (left > 0 ) {
-		dodger.style.left = `${left - 1}px`;
+	if (left > 0) {
+		dodger.style.left = `${left -1}px`;
 	}
 }
 
-function moveDodgerRight () {
-	const rightNumbers = dodger.style.left.replace("px","");
-	const right = parseInt(rightNumbers, 10);
+function moveDodgerRight() {
+	const leftNumbers = dodger.style.left.replace("px", "");
+	const left = parseInt(leftNumbers, 10);
 
-	if (right < 360 ) {
-		dodger.style.left = `${right + 1}px`;
+	if (left < 360) {
+		dodger.style.left = `${left + 1}px`;
 	}
 }
 
-document.addEventListener("keydown", function (e) {
-	if (e.key === "ArrowLeft") {
+document.addEventListener("keydown", function(e) {
+	if(e.key === "ArrowLeft") {
 		moveDodgerLeft();
-	} else if ( e.key === "ArrowRight") {
+	} else if (e.key === "ArrowRight") {
 		moveDodgerRight();
 	}
 });
